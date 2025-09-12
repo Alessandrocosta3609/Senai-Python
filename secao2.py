3fa49b4e5774afe85ac4c1c67cad5ebde09b23dd
Alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-11 21:33:12 -0300

    exercicios seção 2

---
+++ secao2.py
@@ -0,0 +1,42 @@
+print(" Alessandro Costa Ferreira") # Isso é tipo string ......dentro de aspas é texo , string
+print(" 123456 ") # Isso é tipo string ......dentro de aspas é texo , string
+print(123456) # Isso é tipo inteiro  ......sem aspas é numero , inteiro
+print(12.50) # Isso é tipo float ......sem aspas é numero , float
+print(True) # Isso é tipo booleano ......sem aspas é booleano , true or false
+print(False) # Isso é tipo booleano ......sem aspas é booleano , true or false   
+
+print(type(" Alessandro Costa Ferreira")) # Mostra o tipo da variavel
+print(type(" 123456 ")) # Mostra o tipo da variavel         
+print(type(123456)) # Mostra o tipo da variavel
+print(type(12.50)) # Mostra o tipo da variavel
+print(type(True)) # Mostra o tipo da variavel
+print(type(False)) # Mostra o tipo da variavel
+
+
+#Conversão Hexadecimal
+print(0x123)
+print(type(0x123))
+
+#Conversão Octal
+print(0o123)
+print(type(0o123))
+
+#Conversão Binária
+print(0b1010)       
+print(type(0b1010))
+# O prefixo 0b indica que o número está em binário
+# O prefixo 0o indica que o número está em octal
+# O prefixo 0x indica que o número está em hexadecimal
+print(1.)
+print(type(1.))
+
+#Notação Científica
+print(1e3) # 1 * 10^3 = 1000.
+print(type(1e3))
+
+# Booleanos
+print(1>0) # Maior que
+print(1<0) # Menor que  
+
+print (True >= 1) # Nesse caso o True é igual a 1
+print (False <= 0) # Nesse caso o False é igual a 0
