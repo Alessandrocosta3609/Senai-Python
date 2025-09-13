2ab5decf73d003641c83ac9113f22b675e206c59
Alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-12 21:41:41 -0300

    variaveis

---
+++ secao4.py
@@ -0,0 +1,15 @@
+Nome = ("Alessandro")
+Sobrenome = "Ferreira"
+NomeCompleto = Nome + " " + Sobrenome
+Idade = 30
+Altura = 1.66
+print(Altura)
+print(Nome)
+print(Sobrenome) 
+print(NomeCompleto)
+peso = (80)
+print(peso)
+
+
+print ("Meu Nome é ",Nome,"tenho",Idade,"anos, minha altura é",Altura,"e meu peso é",peso)
+
