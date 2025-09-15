bc831591f7b9aa0a85caa576aa7f33254cdee859
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-15 19:50:08 -0300

    correção exercicio

--- secao4.py
+++ secao4.py
@@ -1,3 +1,4 @@
+# Trabalhando com Variaveis
 Nome = ("Alessandro")
 Sobrenome = "Ferreira"
 NomeCompleto = Nome + " " + Sobrenome
@@ -13,6 +14,9 @@ print(peso)
 
 print ("Meu Nome é ",Nome,"tenho",Idade,"anos, minha altura é",Altura,"e meu peso é",peso ,"kg")
 
+
+# Contadores
+
 cont = 1
 print (cont)
 cont = cont + 1
@@ -21,3 +25,22 @@ cont += 1
 print (cont)
 cont -= 1
 print (cont)
+
+# Teorema de Pitagoras
+a = 25
+b = 50
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", round (c,2) )
+ 
+
+# Exercicio 
+# João tem 3 maçãs , Maria tem 5 maçãs e Ana tem 5 maçãs.
+# Mostre a quantidade que cada um tem e o total de maçãs.
+
+j = 3
+m = 5
+a = 6
+total = (j+m+a)
+
+print( " Joao tem ", j ,"macas \n","Maria tem " , m ,"macas\n" , "Ana tem ",a ,"macas\n")
+print ( " Ao todo sao", total , "macas")
