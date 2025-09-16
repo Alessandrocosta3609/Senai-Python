b02d9ce16dcb7ca5f566f67c809552bddd1b7f67
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-15 21:28:46 -0300

    correções2

--- secao6.py
+++ secao6.py
@@ -1,8 +1,28 @@
+#ctrl + ; comenta a linha       
+#ctrl +shift + k deleta a linha
+#ctrl + alt + seta para cima ou para baixo move a linha 
+#shift + alt + seta para cima ou para baixo copia a linha
+#ctrl + d seleciona a palavra
+
+
 # Trabalhando com Variáveis Parte 2
 
-nome = input ("Digite seu nome : ")
+"""nome = input ("Digite seu nome : ")
 idade = input ("Qual a sua idade ?  : ")
 peso = input ( "Qual o seu peso ?  : ")
 print("Olá", nome)
 print ("Legal",nome,"muito prazer em te conhecer !")
+print (nome, "\nVocê tem", idade ,"anos" "\nE pesa", peso ,"kg)"""
+
+# Fim do exercicio
+
+
+# Forçar os tipos de variaveis
+
+nome = input ("Digite seu nome : ")
+idade = int(input ("Qual a sua idade ?  : "))
+peso = float(input ( "Qual o seu peso ?  : "))
+print("Olá", nome)
+print ("Legal",nome,"muito prazer em te conhecer !")
 print (nome, "\nVocê tem", idade ,"anos" "\nE pesa", peso ,"kg")
+
