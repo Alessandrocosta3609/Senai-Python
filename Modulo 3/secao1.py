d47426faa613742b50ca3cfadafb1af8a63c86ce
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-16 19:35:44 -0300

    sinais de operadores

---
+++ Modulo 3/secao1.py
@@ -0,0 +1,14 @@
+# Decisões e Comparações
+# O simbolo de = serve para atribuir uma variavel
+# Se quiser comparar 2 valores , o simbolo de igualdade no python é ==
+# se quiser compara diferenças , o simbolo de diferente é  !=
+
+print(1 !=0) # 1 é diferente de zero ? True
+print(1 == 0)# 1 é igual a zero ? True
+print(2 > 1) # 2 é maior que 1 ? True
+print(2 < 1) # 2 é menor que 1 ? False
+print(2 >= 1) # 2 é maior ou igual a 1 ? True
+print(2 <= 1) # 2 é menor ou igual a 1 ? False
+print('a' == 'b') # 'a' é igual a 'b' ? False
+print('a' != 'b') # 'a' é diferente de 'b' ? True
+
