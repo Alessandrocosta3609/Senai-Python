67f900dde47898c323e4acec5696edca0eabfa3c
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-15 21:14:51 -0300

    variaveis pt2

---
+++ secao6.py
@@ -0,0 +1,8 @@
+# Trabalhando com Variáveis Parte 2
+
+nome = input ("Digite seu nome : ")
+idade = input ("Qual a sua idade ?  : ")
+peso = input ( "Qual o seu peso ?  : ")
+print("Olá", nome)
+print ("Legal",nome,"muito prazer em te conhecer !")
+print (nome, "\nVocê tem", idade ,"anos" "\nE pesa", peso ,"kg")
