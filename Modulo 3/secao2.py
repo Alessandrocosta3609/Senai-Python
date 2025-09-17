02b028642cd70d3214938ad673fd7624b551b5cd
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-17 20:54:56 -0300

    loop com soma

--- Modulo 3/secao2.py
+++ Modulo 3/secao2.py
@@ -8,3 +8,17 @@
 #     print("Palmeiras não tem Mundial")
 #     cont += 1
 #Exemplo de Loop controlado
+# senha = ""
+# while senha != "1234":
+#     senha = input("Digite a senha : ")
+# print("Acesso permitido")
+# #Exemplo de Loop com senha
+
+# soma = 0
+# n = int(input("Digite um valor (0 para sair) : "))
+# while n != 0:
+#     soma += n
+#     n = int(input("Digite um valor (0 para sair) : "))
+# print("A soma dos valores é : ", soma)
+# Exemplo de Loop com soma de valores
+
