6c1c550b9c6c588d4865f618fc472ce5f297a6b5
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-17 19:56:59 -0300

    loops

---
+++ Modulo 3/secao2.py
@@ -0,0 +1,10 @@
+# while True:
+#     print("Palmeiras não  tem Mundial")
+#Exemplo de Loop Infinito
+
+
+# cont = 1
+# while cont <= 10:
+#     print("Palmeiras não tem Mundial")
+#     cont += 1
+#Exemplo de Loop controlado
