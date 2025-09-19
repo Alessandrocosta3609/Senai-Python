289b7b01026ac74456d409ba625c3c7bb0d03137
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-18 21:49:07 -0300

    array

---
+++ Modulo 3/secao4.py
@@ -0,0 +1,17 @@
+# Array
+
+# lista = ["Alessandro ","Natan","Carlos","Camila"]
+
+# print(lista[0]) # Para imprimir  posição especifica da lista , começando com 0
+
+# print(lista[1])
+
+# print(len(lista)) # Para saber o tamanho da lista
+
+
+# numbers = [10,5,7,2,1]
+# print(len(numbers))
+# print(numbers[0])
+
+# numbers[1] = 200
+# print(numbers)
