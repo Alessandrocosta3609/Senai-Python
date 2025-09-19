71b187877d18c9b36bdab18f5f99936e69abb810
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-18 21:25:30 -0300

    binarios

---
+++ Modulo 3/secao3.py
@@ -0,0 +1,41 @@
+# Operadores Logicos
+
+# var =1
+# print (not( var >0))
+# print ( not(var < 0))
+
+# print(var != 0)
+# print(not(var ==0))
+
+# c = 10
+# v = 101
+
+# if c > 0 or v == 100:
+#  print("Condição Correta")
+
+
+
+
+# a = False
+# b = True
+
+# if a or b :
+#     print("Uma das Variaveis é Verdadeira")
+# else :
+#     print("As 2 são falsas")    
+# # Tabela Verdade OR
+
+# i = 1
+# j = not not i
+# print(j)
+
+# Resultado = 1
+
+
+a =5
+b =3
+print(a & b) #1   -> 0001
+print(a | b) #7   -> 0111
+print(a ^ b) #6   -> 0110
+print ( ~(a & b)) #-2  -> 0010
+
