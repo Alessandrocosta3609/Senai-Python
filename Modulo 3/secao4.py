9ad0951bc15050513f35213a4001b4eb26649f55
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-19 21:00:17 -0300

    fechamento seção 4

--- Modulo 3/secao4.py
+++ Modulo 3/secao4.py
@@ -10,8 +10,60 @@
 
 
 # numbers = [10,5,7,2,1]
-# print(len(numbers))
 # print(numbers[0])
 
 # numbers[1] = 200
+# del numbers[0]
+
+# numbers[0] = 44
 # print(numbers)
+# print(len(numbers))
+
+# hat_list = [1,2,3,4,5]
+# print(len(hat_list))
+# hat_list[2]=9
+# print(hat_list)
+# del hat_list[3]
+# print(hat_list)
+
+# list = [1,2,4,5]
+# print(list)
+# list.append(6) # Acrecenta valor na ultima posição da lista
+# print(list)
+# list.insert(2,3) # Acrescenta valor em qualquer posição da lista 
+# print(list)
+# print(len(list)) # mostra o tamanho da lista
+
+# lista = []
+# for i in range(100):
+#     lista.append (i + 1)
+# print(lista)
+#cria uma lista vazia e adiciona 100 numeros automaticamente , 
+
+
+# lista = []
+# for i in range(10):
+#     lista.insert(0,i+1)
+# print(lista)
+
+# Cria uma lista e adiciona 10 numeros em ordem decrescente
+
+# lst = [1, 2, 3, 4, 5]
+# lst.insert(1, 6)
+# del lst[0]
+# lst.append(1)
+ 
+# print(lst)
+
+# lista = [10,1,8,3,5]
+# total =0
+# for i in lista:
+#     total += i
+# print(total)
+ # Soma os valores de uma lista
+
+
+# lista = [1,2,[3,4],5,6]
+# print(lista) 
+# Da pra ter uma lista dentro de outra lista
+
