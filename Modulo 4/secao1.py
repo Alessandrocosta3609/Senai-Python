b8f84f459fa27c4ad64640ed7e39fe246d2286ee
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-09-22 21:52:32 -0300

    modulo 4

---
+++ Modulo 4/secao1.py
@@ -0,0 +1,17 @@
+# Função
+
+print ("Entre com um Valor :")
+
+a = int(input())
+
+print ("Entre com um Valor :")
+
+b = int(input())
+
+print ("Entre com um Valor :")
+
+c = int(input())
+
+print ("Entre com um Valor :")
+
+d = int(input())
